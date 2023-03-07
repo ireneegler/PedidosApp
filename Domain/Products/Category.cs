e8590044ef3bc3b033da416cefd6be4c3976f9e1
Irene Egler <irene.egler@gmail.com>
2023-03-07 12:03:36 -0300

    Create Class Category and Products

---
+++ Domain/Products/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace PedidosApp.Domain.Products;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string CreateBy { get; set; }
+    public DateTime CreateOn { get; set; }
+    public DateTime EditedOn { get; set; }
+}
+
---
+++ Domain/Products/Products.cs
@@ -0,0 +1,14 @@
+﻿namespace PedidosApp.Domain.Products;
+
+public class Products
+{
+    public Guid Id { get; set; }
+    public string Name { get; set;}
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public string CreateBy { get; set; }
+    public DateTime CreateOn { get; set; }
+    public DateTime EditedOn { get; set; }
+}
+
